f8dbf40523634ce870c236dbd750a9898f9e6ce5
wdostert <wdostert@live.com>
2023-12-10 15:11:03 -0500

    Added search

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import datetime
 import os
-import sqlite3, secrets
 import sqlite3, secrets
 from flask import Flask, render_template, request, redirect, url_for, session
 from distutils.log import debug 
@@ -10,6 +8,8 @@ from datetime import datetime as dt
 current_user_id = -1
 
 app = Flask(__name__)
+secret_key = secrets.token_urlsafe(32)
+app.secret_key = secret_key 
 secret_key = secrets.token_urlsafe(32)
 app.secret_key = secret_key 
 
@@ -29,15 +29,6 @@ def create_table():
             content TEXT NOT NULL,
             category TEXT NOT NULL,
             posted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (author_id) REFERENCES users(id)
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS replies (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            author_id INTEGER,
-            content TEXT NOT NULL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY (author_id) REFERENCES users(id)
         )
     ''')
@@ -62,31 +53,11 @@ def get_posts():
     conn.close()
     return posts
 
-def get_post(id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    # Fetch posts from the database
-    cursor.execute('''
-        SELECT posts.id, posts.title, posts.content, posts.category, posts.author_id, users.username
-        FROM posts
-        JOIN users ON posts.author_id = users.id
-        WHERE posts.id = ?
-    ''', (id,))    
-    post = cursor.fetchone()
-
-    conn.close()
-    return post
-
 @app.route('/index.html')
 def index():
-
     session['signed_up'] = False
     posts = get_posts()
-    u=''
-    if 'username' in session.keys():
-        u=session['username']
-    return render_template('index.html', items=posts, username=u)
+    return render_template('index.html', items=posts)
 
 @app.route('/')
 def root():
@@ -118,23 +89,16 @@ def create_post():
         conn.commit()
         conn.close()
 
-    return render_template('post.html')
+    return render_template('new-post.html')
 
 @app.route('/overview-forum-category.html')
 def overviewforumcategory():
     return render_template('overview-forum-category.html')
 
 
-@app.route('/post.html/<int:pid>')
-def post(pid=None):
-    conn = get_db_connection()
-    cursor=conn.cursor()
-    post = cursor.execute(f"SELECT * FROM posts WHERE id={pid}").fetchone()
-    conn.commit()
-    return render_template('post.html', post=post)
-
-pid = '/pid'
-app.add_url_rule(pid, 'post', post)
+@app.route('/post.html')
+def post():
+    return render_template('post.html')
 
 @app.route('/sign-up.html')
 def signup():
@@ -188,7 +152,7 @@ def loginpost():
         # Set the user ID in the session upon successful login
         session['user_id'] = current_user_id
         session['signed_up'] = True
-        session['username'] = username
+
         return redirect(url_for('profile'))
     else:
         session['signed_up'] = False
@@ -196,8 +160,8 @@ def loginpost():
 
 @app.route('/logout')
 def logout():
-    # Remove the user ID and any other session variables upon logout
-    session.clear()
+    # Remove the user ID from the session upon logout
+    session.pop('user_id', None)
     return redirect(url_for('login'))
 
 @app.route('/profile.html')
@@ -235,13 +199,6 @@ def success():
 def search():
     query = request.args.get('query')
 
-
-@app.route('/view-post/<int:id>')
-def view_post(id):
-    post = get_post(id)
-    print(id)
-    return render_template('view-post.html', post=post)
-
     if query:
         con = get_db_connection()
         cursor = con.cursor()
@@ -256,7 +213,6 @@ def view_post(id):
         return render_template('search_results.html', query=query, results=search_results)
     else:
         return render_template('search_results.html', query=None, results=None)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
